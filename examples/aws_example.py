b58fd3b63f02113e695a9879fbe33015da9b31b1
AJ Dehn <ajdehn76@gmail.com>
2026-06-24 09:28:06 -0500

    Adding uploader.

--- examples/aws_example.py
+++ examples/aws_example.py
@@ -1,9 +1,14 @@
-import boto3
-
-from auditops import AWSCollector, EvidenceWriter
+import boto3, shutil
+from auditops import AWSCollector, EvidenceWriter, Uploader
 
 session = boto3.Session()
 writer = EvidenceWriter()
-
 collector = AWSCollector(session)
 collector.collect(writer)
+
+# Create zip file
+shutil.make_archive("audit_evidence", "zip", "audit_evidence")
+
+# Upload evidence to audit portal
+uploader = Uploader("https://upload.auditops.io")
+#uploader.upload("audit_evidence.zip", "john.doe@client.com", "jane.doe@auditor.com")
--- src/auditops/__init__.py
+++ src/auditops/__init__.py
@@ -1,9 +1,11 @@
 __version__ = "0.1.0"
 
 from .evidence_writer import EvidenceWriter
-from .collectors.aws import AWSCollector
+from .collectors.aws_collector import AWSCollector
+from .uploader import Uploader
 
 __all__ = [
     "EvidenceWriter",
-    "AWSCollector"
+    "AWSCollector",
+    "Uploader"
 ]
