2d69981ce462531ca6dfe0d00d845d616a4c5fe1
AJ Dehn <ajdehn76@gmail.com>
2026-07-01 21:16:52 -0500

    Add S3 Public test.

--- src/auditops/providers/aws/aws_tester.py
+++ src/auditops/providers/aws/aws_tester.py
@@ -22,6 +22,7 @@ class AWSTester:
             self._run_test(self._test_iam_root_access_key),
             self._run_test(self._test_iam_password_policy),
             self._run_test(self._test_s3_encryption),
+            self._run_test(self._test_s3_public_access),
             #self._test_ec2_security_group_tags()
         ]
 
@@ -29,7 +30,6 @@ class AWSTester:
         test = test_func()
 
         exclusion = self.exclusions.get_test_exclusion(self.provider, test.test_id)
-        print(f"Test: {test.test_id}. Excluded: {exclusion}")
         if exclusion:
             test.is_excluded = True
             test.comments = exclusion.rationale
@@ -150,6 +150,51 @@ class AWSTester:
 
         if failures:
             test.comments = " ".join(failures)
+
+        return test
+
+    def _test_s3_public_access(self):
+        metadata = {
+            "id": "AWS-S3-002",
+            "description": "S3 buckets are configured to block public access.",
+            "risk_rating": 2,
+            "headers": ["Bucket Name", "Result", "Comments"],
+            "attributes": [
+                "BlockPublicAcls, IgnorePublicAcls, BlockPublicPolicy, and RestrictPublicBuckets are set to true."
+            ],
+            "procedures": [
+                "Obtained a list of S3 buckets by calling the list_buckets() boto3 command.",
+                "Saved the list of buckets: s3/buckets.json.",
+                "For each bucket, obtained the public access block settings by calling the get_public_access_block() boto3 command.",
+                "For each bucket, saved the public access block settings: s3/buckets/[bucket_name]/public_access_block.json.",
+                "For each bucket, inspected the public access block settings to determine if they comply with the test attribute(s) below."
+            ]      
+        }
+        test = self._create_test(metadata)
+
+        buckets = self._read("s3/buckets.json")
+
+        for bucket in buckets.get("Buckets", []):
+            bucket_name = bucket["Name"]
+            sample = Sample(sample_id={"bucket_name": bucket_name})
+
+            public_access_block = self._read(f"s3/buckets/{bucket_name}/public_access_block.json")
+            
+            if not public_access_block:
+                sample.comments = "No Public Access Block configuration found."
+                test.samples.append(sample)
+                continue
+
+            config = public_access_block.get("PublicAccessBlockConfiguration", {})
+            sample.is_passing = all([config.get("BlockPublicAcls", False), config.get("IgnorePublicAcls", False),
+            config.get("BlockPublicPolicy", False), config.get("RestrictPublicBuckets", False)])
+
+            if not sample.is_passing:
+                sample.comments = "One or more public access settings are disabled."
+
+            test.samples.append(sample)
+
+        test.evaluate_samples(self.exclusions, self.provider)
 
         return test
 
