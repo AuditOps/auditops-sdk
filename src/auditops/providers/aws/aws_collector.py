c5a0fb39375f23febef3efcd87d6567d7772ad12
AJ Dehn <ajdehn76@gmail.com>
2026-07-01 21:15:55 -0500

    Add call_api

--- src/auditops/providers/aws/aws_collector.py
+++ src/auditops/providers/aws/aws_collector.py
@@ -6,14 +6,33 @@ class AWSCollector:
         self.config = config
 
     def collect(self, writer):
-        """
-        Collect all AWS evidence.
-        """
         self._collect_account_identity(writer)
         self._collect_iam_evidence(writer)
         self._collect_s3_evidence(writer)
         self._collect_ec2_evidence(writer)
 
+    def _call_api(self, client, method=None, method_kwargs=None, paginator_params=None,
+                ignore_codes=None, warn_codes=None):
+        try:
+            if paginator_params:
+                paginator = client.get_paginator(paginator_params["method_name"])
+                key = paginator_params["pagination_key"]
+                items = []
+                for page in paginator.paginate(**(paginator_params.get("params") or {})):
+                    items.extend(page.get(key, []))
+                return {key: items}
+
+            return getattr(client, method)(**(method_kwargs or {}))
+
+        except ClientError as e:
+            code = e.response["Error"]["Code"]
+            if warn_codes and code in warn_codes:
+                logger.warning(f"{code} calling {method or paginator_params.get('method_name')}")
+                return None
+            if ignore_codes and code in ignore_codes:
+                return None
+            raise
+        
     def _collect_account_identity(self, writer):
         sts_client = self.session.client("sts")
         identity = sts_client.get_caller_identity()
@@ -33,23 +52,24 @@ class AWSCollector:
 
     def _collect_s3_evidence(self, writer):
         s3_client = self.session.client("s3")
-        buckets = s3_client.list_buckets()
-
+        buckets = self._call_api(s3_client, method="list_buckets")
         writer.save_json("aws", "s3/buckets.json", buckets)
+
         for bucket in buckets.get("Buckets", []):
-            bucket_name = bucket['Name']
-            try:
-                bucket_encryption = s3_client.get_bucket_encryption(Bucket=bucket_name)
-                writer.save_json("aws", f"s3/buckets/{bucket_name}/encryption.json", bucket_encryption)
-            except ClientError as e:
-                if e.response['Error']['Code'] == 'ServerSideEncryptionConfigurationNotFoundError':
-                    # Encryption is not enabled. This will show as a finding on the report.
-                    pass
-                elif e.response['Error']['Code'] == 'AccessDenied':
-                    # Unable to gather evidence. Will need to be manually investigated.
-                    logger.warning(f"AccessDenied when calling get_bucket_encryption(): {bucket_name}")
-                else:
-                    raise e
+            name = bucket["Name"]
+
+            encryption = self._call_api(
+                s3_client, method="get_bucket_encryption", method_kwargs={"Bucket": name},
+                ignore_codes=["ServerSideEncryptionConfigurationNotFoundError"],
+                warn_codes=["AccessDenied"],
+            )
+            writer.save_json("aws", f"s3/buckets/{name}/encryption.json", encryption)
+
+            public_access_block = self._call_api(
+                s3_client, method="get_public_access_block", method_kwargs={"Bucket": name},
+                ignore_codes=["NoSuchPublicAccessBlockConfiguration"],
+            )
+            writer.save_json("aws", f"s3/buckets/{name}/public_access_block.json", public_access_block)
 
     def _collect_ec2_evidence(self, writer):
         for region in self.config.in_scope_regions:
