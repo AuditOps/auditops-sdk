44ed2a6fc92e6f747701e03b138f13db52962c38
AJ Dehn <ajdehn76@gmail.com>
2026-07-02 16:14:19 -0500

    Refactoring evidence collection.

--- src/auditops/providers/aws/aws_collector.py
+++ src/auditops/providers/aws/aws_collector.py
@@ -12,6 +12,13 @@ class AWSCollector:
         self._collect_iam_evidence()
         self._collect_s3_evidence()
         self._collect_ec2_evidence()
+        self._collect_lambda_evidence()
+        self._collect_rds_evidence()
+        self._collect_cloudtrail_evidence()
+        self._collect_elbv2_evidence()
+        self._collect_apigateway_evidence()
+        self._collect_wafv2_evidence()
+        self._collect_guardduty_evidence()
 
     def _call_api(self, client, method=None, method_kwargs=None, paginator_params=None,
                 ignore_codes=None, warn_codes=None):
@@ -43,74 +50,382 @@ class AWSCollector:
         identity = sts_client.get_caller_identity()
         self._write("account/account_identity.json", identity)
 
+    ####################################################################
+    #
+    # IAM
+    #
+    ####################################################################
+
     def _collect_iam_evidence(self):
         iam_client = self.session.client("iam")
         self._write("iam/account_summary.json", iam_client.get_account_summary())
+
         try:
-            password_policy = iam_client.get_account_password_policy()
-            self._write("iam/password_policy.json", password_policy)
+            self._write(
+                "iam/password_policy.json",
+                iam_client.get_account_password_policy(),
+            )
+
         except ClientError as e:
-            if e.response["Error"]["Code"] == "NoSuchEntity":
-                pass
-            else:
+            if e.response["Error"]["Code"] != "NoSuchEntity":
                 raise
 
-        # Users
+        self._collect_iam_users(iam_client)
+        self._collect_iam_roles(iam_client)
+        self._collect_iam_groups(iam_client)
+
+    def _collect_iam_users(self, iam_client):
         users = self._call_api(iam_client,
             paginator_params={"method_name": "list_users", "pagination_key": "Users"},
         )
         self._write("iam/users.json", users)
 
-        for user in users.get("Users"):
+        for user in users.get("Users", []):
             user_name = user["UserName"]
 
-            login_profile = self._call_api(iam_client, method="get_login_profile", method_kwargs={"UserName": user_name},
-            ignore_codes=["NoSuchEntity"])
-            self._write(f"iam/users/{user_name}/login_profile.json", login_profile)
+            self._write(
+                f"iam/users/{user_name}/login_profile.json",
+                self._call_api(
+                    iam_client,
+                    method="get_login_profile",
+                    method_kwargs={
+                        "UserName": user_name,
+                    },
+                    ignore_codes=["NoSuchEntity"],
+                ),
+            )
 
-            mfa_devices = self._call_api(iam_client, method="list_mfa_devices", method_kwargs={"UserName": user_name})
-            self._write(f"iam/users/{user_name}/mfa_devices.json", mfa_devices)
+            self._write(
+                f"iam/users/{user_name}/mfa_devices.json",
+                self._call_api(
+                    iam_client,
+                    method="list_mfa_devices",
+                    method_kwargs={
+                        "UserName": user_name,
+                    },
+                ),
+            )
 
-        # Roles
-        roles = self._call_api(iam_client,
-            paginator_params={"method_name": "list_roles", "pagination_key": "Roles"},
+            self._write(
+                f"iam/users/{user_name}/access_keys.json",
+                self._call_api(
+                    iam_client,
+                    method="list_access_keys",
+                    method_kwargs={
+                        "UserName": user_name,
+                    },
+                ),
+            )
+
+    def _collect_iam_roles(self, iam_client):
+        self._write(
+            "iam/roles.json",
+            self._call_api(
+                iam_client,
+                paginator_params={
+                    "method_name": "list_roles",
+                    "pagination_key": "Roles",
+                },
+            ),
         )
-        self._write("iam/roles.json", roles)
 
-        # Groups
-        groups = self._call_api(
-            iam_client,
-            paginator_params={"method_name": "list_groups", "pagination_key": "Groups"}
+    def _collect_iam_groups(self, iam_client):
+        self._write(
+            "iam/groups.json",
+            self._call_api(
+                iam_client,
+                paginator_params={
+                    "method_name": "list_groups",
+                    "pagination_key": "Groups",
+                },
+            ),
         )
-        self._write("iam/groups.json", groups)
-        
+
+    ####################################################################
+    #
+    # S3
+    #
+    ####################################################################
 
     def _collect_s3_evidence(self):
         s3_client = self.session.client("s3")
+
         buckets = self._call_api(s3_client, method="list_buckets")
         self._write("s3/buckets.json", buckets)
 
         for bucket in buckets.get("Buckets", []):
-            name = bucket["Name"]
-
-            encryption = self._call_api(
-                s3_client, method="get_bucket_encryption", method_kwargs={"Bucket": name},
-                ignore_codes=["ServerSideEncryptionConfigurationNotFoundError"],
-                warn_codes=["AccessDenied"],
+            bucket_name = bucket["Name"]
+            # Encryption
+            self._write(
+                f"s3/buckets/{bucket_name}/encryption.json",
+                self._call_api(
+                    s3_client,
+                    method="get_bucket_encryption",
+                    method_kwargs={"Bucket": bucket_name},
+                    ignore_codes=["ServerSideEncryptionConfigurationNotFoundError"],
+                    warn_codes=["AccessDenied"],
+                )
             )
-            self._write(f"s3/buckets/{name}/encryption.json", encryption)
-
-            public_access_block = self._call_api(
-                s3_client, method="get_public_access_block", method_kwargs={"Bucket": name},
-                ignore_codes=["NoSuchPublicAccessBlockConfiguration"],
+            # Public access block
+            self._write(
+                f"s3/buckets/{bucket_name}/public_access_block.json",
+                self._call_api(
+                    s3_client,
+                    method="get_public_access_block",
+                    method_kwargs={"Bucket": bucket_name},
+                    ignore_codes=["NoSuchPublicAccessBlockConfiguration"],
+                ),
             )
-            self._write(f"s3/buckets/{name}/public_access_block.json", public_access_block)
+            # Bucket policy
+            self._write(
+                f"s3/buckets/{bucket_name}/policy.json",
+                self._call_api(
+                    s3_client,
+                    method="get_bucket_policy",
+                    method_kwargs={"Bucket": bucket_name},
+                    ignore_codes=["NoSuchBucketPolicy"],
+                    warn_codes=["AccessDenied"],
+                ),
+            )
+            # Bucket tags
+            self._write(
+                f"s3/buckets/{bucket_name}/tags.json",
+                self._call_api(
+                    s3_client,
+                    method="get_bucket_tagging",
+                    method_kwargs={"Bucket": bucket_name},
+                    ignore_codes=["NoSuchTagSet"],
+                    warn_codes=["AccessDenied"],
+                ),
+            )
+
+    ####################################################################
+    #
+    # EC2 / EBS
+    #
+    ####################################################################
 
     def _collect_ec2_evidence(self):
         for region in self.config.in_scope_regions:
-            try:
-                ec2_client = self.session.client("ec2", region_name=region)
-                instances = ec2_client.describe_instances()
-                self._write(f"ec2/{region}/instances.json", instances)
-            except ClientError as e:
-                raise
+            ec2_client = self.session.client(
+                "ec2",
+                region_name=region,
+            )
+            self._collect_ec2_instances(ec2_client,region)
+            self._collect_ec2_security_groups(ec2_client,region)
+            self._collect_ebs_volumes(ec2_client,region)
+            self._collect_ebs_default_encryption(ec2_client,region)
+
+    def _collect_ec2_instances(self, ec2_client, region):
+        self._write(
+            f"ec2/{region}/instances.json",
+            self._call_api(
+                ec2_client,
+                method="describe_instances",
+            ),
+        )
+
+    def _collect_ec2_security_groups(self, ec2_client, region):
+        self._write(
+            f"ec2/{region}/security_groups.json",
+            self._call_api(
+                ec2_client,
+                method="describe_security_groups",
+            ),
+        )
+
+    def _collect_ebs_volumes(self, ec2_client, region):
+        self._write(
+            f"ec2/{region}/volumes.json",
+            self._call_api(
+                ec2_client,
+                method="describe_volumes",
+            ),
+        )
+
+    def _collect_ebs_default_encryption(self, ec2_client, region):
+        self._write(
+            f"ec2/{region}/ebs_encryption_by_default.json",
+            self._call_api(
+                ec2_client,
+                method="get_ebs_encryption_by_default",
+            ),
+        )
+    ####################################################################
+    #
+    # Lambda
+    #
+    ####################################################################
+
+    def _collect_lambda_evidence(self):
+        lambda_client = self.session.client("lambda")
+
+        functions = self._call_api(lambda_client, method="list_functions")
+        self._write("lambda/functions.json", functions)
+
+        for function in functions.get("Functions", []):
+            function_name = function["FunctionName"]
+            self._write(
+                f"lambda/functions/{function_name}/tags.json",
+                self._call_api(
+                    lambda_client,
+                    method="list_tags",
+                    method_kwargs={
+                        "Resource": function["FunctionArn"],
+                    },
+                ),
+            )
+
+    ####################################################################
+    #
+    # RDS
+    #
+    ####################################################################
+
+    def _collect_rds_evidence(self):
+        rds_client = self.session.client("rds")
+
+        self._write(
+            "rds/db_instances.json",
+            self._call_api(
+                rds_client,
+                method="describe_db_instances",
+            ),
+        )
+
+        self._write(
+            "rds/db_clusters.json",
+            self._call_api(
+                rds_client,
+                method="describe_db_clusters",
+            ),
+        )
+
+    ####################################################################
+    #
+    # CloudTrail
+    #
+    ####################################################################
+
+    def _collect_cloudtrail_evidence(self):
+        cloudtrail_client = self.session.client("cloudtrail")
+
+        trails = self._call_api(
+            cloudtrail_client,
+            method="describe_trails",
+        )
+
+        self._write(
+            "cloudtrail/trails.json", 
+            trails)
+
+        for trail in trails.get("trailList", []):
+            trail_name = trail["Name"]
+
+            self._write(
+                f"cloudtrail/{trail_name}/status.json",
+                self._call_api(
+                    cloudtrail_client,
+                    method="get_trail_status",
+                    method_kwargs={
+                        "Name": trail_name,
+                    },
+                ),
+            )
+
+    ####################################################################
+    #
+    # ELBv2
+    #
+    ####################################################################
+
+    def _collect_elbv2_evidence(self):
+        elbv2_client = self.session.client("elbv2")
+
+        self._write(
+            "elbv2/load_balancers.json",
+            self._call_api(
+                elbv2_client,
+                method="describe_load_balancers",
+            ),
+        )
+
+    ####################################################################
+    #
+    # API Gateway
+    #
+    ####################################################################
+
+    def _collect_apigateway_evidence(self):
+        apigateway_client = self.session.client("apigateway")
+
+        self._write(
+            "apigateway/rest_apis.json",
+            self._call_api(
+                apigateway_client,
+                method="get_rest_apis",
+            ),
+        )
+
+    ####################################################################
+    #
+    # WAFv2
+    #
+    ####################################################################
+
+    def _collect_wafv2_evidence(self):
+        wafv2_client = self.session.client("wafv2")
+
+        web_acls = self._call_api(
+            wafv2_client,
+            method="list_web_acls",
+            method_kwargs={
+                "Scope": "REGIONAL",
+            },
+        )
+
+        self._write("wafv2/web_acls.json", web_acls)
+
+        for web_acl in web_acls.get("WebACLs", []):
+            web_acl_id = web_acl["Id"]
+            web_acl_name = web_acl["Name"]
+
+            self._write(
+                f"wafv2/{web_acl_name}_{web_acl_id}/resources.json",
+                self._call_api(
+                    wafv2_client,
+                    method="list_resources_for_web_acl",
+                    method_kwargs={
+                        "WebACLArn": web_acl["ARN"],
+                    },
+                ),
+            )
+
+
+    ####################################################################
+    #
+    # GuardDuty
+    #
+    ####################################################################
+
+    def _collect_guardduty_evidence(self):
+        guardduty_client = self.session.client("guardduty")
+
+        detectors = self._call_api(
+            guardduty_client,
+            method="list_detectors",
+        )
+
+        self._write("guardduty/detectors.json", detectors)
+
+        for detector_id in detectors.get("DetectorIds", []):
+            self._write(
+                f"guardduty/{detector_id}/detector.json",
+                self._call_api(
+                    guardduty_client,
+                    method="get_detector",
+                    method_kwargs={
+                        "DetectorId": detector_id,
+                    },
+                ),
+            )
--- src/auditops/providers/aws/aws_config.py
+++ src/auditops/providers/aws/aws_config.py
@@ -2,22 +2,44 @@ from dataclasses import dataclass, field
 
 @dataclass
 class AWSConfig:
-    # Required Field
+    """
+    Configuration options used by AWSCollector and AWSTester.
+
+    Most values represent your organization's desired security baseline.
+    Tests compare collected AWS evidence against these values.
+    """
+
+    # Required
     in_scope_regions: list[str]
 
-    # Tagging
-    required_tags: list[str] = field(
-        default_factory=lambda: ["Owner", "Description", "Classification"]
-    )
+    # Default tagging policy
+    required_tags: list[str] = field(default_factory=lambda: ["Owner", "Description", "Classification"])
+    ec2_security_group_required_tags: list[str] = field(default_factory=lambda: ["Owner","Description","ReviewedBy","LastReviewedDate"])    
+
+    # Optional service-specific tag overrides
+    s3_required_tags: list[str] | None = None
+    ec2_required_tags: list[str] | None = None
+    ebs_required_tags: list[str] | None = None
+    lambda_required_tags: list[str] | None = None
+    rds_required_tags: list[str] | None = None
 
     # IAM Password Policy
-    iam_minimum_password_length: int = 14
+    iam_minimum_password_length: int = 16
     iam_require_uppercase: bool = True
     iam_require_lowercase: bool = True
     iam_require_numbers: bool = True
     iam_require_symbols: bool = True
     iam_max_password_age: int = 0               # NOTE: Value of 0 means passwords do not need to be rotated.
     iam_password_reuse_prevention: int = 24
+
+    # IAM Access Keys
+    iam_access_key_max_age: int = 90
+
+    # RDS
+    rds_backup_retention_days: int = 14
+
+    # CloudTrail
+    cloudtrail_logging_lookback_days: int = 365
 
     def __post_init__(self):
         if not self.in_scope_regions:
@@ -27,11 +49,28 @@ class AWSConfig:
         self.in_scope_regions = list(dict.fromkeys(
             region.lower().strip()
             for region in self.in_scope_regions
-        ))        
+        ))
 
     def update(self, **kwargs):
+        # Allow end users to update configuration values after construction.
         for key, value in kwargs.items():
             if not hasattr(self, key):
                 raise ValueError(f"Unknown configuration option: {key}")
 
             setattr(self, key, value)
+
+    def get_required_tags(self, service: str) -> list[str]:
+        """
+        Returns the required tags for a particular AWS service.
+        Falls back to required_tags if no service-specific override exists.
+        """
+
+        override = {
+            "s3": self.s3_required_tags,
+            "ec2": self.ec2_required_tags,
+            "ebs": self.ebs_required_tags,
+            "lambda": self.lambda_required_tags,
+            "rds": self.rds_required_tags,
+        }.get(service)
+
+        return override or self.required_tags
