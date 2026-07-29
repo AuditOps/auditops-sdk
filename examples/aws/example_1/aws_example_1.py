e006330e168af6cc94e6a86dfc0b91efad45d377
AJ Dehn <ajdehn76@gmail.com>
2026-07-29 07:22:45 -0500

    Updating code examples for audit.run()

--- examples/aws/example_1/aws_example_1.py
+++ examples/aws/example_1/aws_example_1.py
@@ -9,17 +9,27 @@
 
 from auditops.core.models import Audit, AuditHelpers
 from auditops.providers.aws import AWSCollector, AWSTester, AWSConfig
-from auditops.core.utils import aws_create_session, run_audit
+from auditops.core.utils import aws_create_session
 
 def main():
     session = aws_create_session()
     aws_config = AWSConfig(in_scope_regions=['us-east-1'])
     helpers = AuditHelpers.create()
 
-    audit = Audit(helpers = helpers, title = "AWS Audit Report", config=aws_config,
-    auditor_name = "AJ Dehn", evidence_folder = "aws")
+    # NOTE: Setting summary_mode to 'True' will anonymize the sample ID's in the PDF & JSON reports (ex. "Sample 1", "Sample 2", "Sample 3").
+    # NOTE: Setting delete_cached_evidence to 'True' will allow you to use previously collected evidence.
+    audit = Audit(helpers = helpers, title = "AWS Audit Report", config=aws_config, auditor_name = "AJ Dehn",
+    evidence_folder = "aws", delete_cached_evidence=False, summary_mode=True, exclusions=None)
 
-    run_audit(audit, AWSCollector(session, audit), AWSTester(audit))
+    audit.run(collector=AWSCollector(session), tester=AWSTester())
+
+    """
+    audit.collect(collector=AWSCollector(session))
+    audit.perform_testing(tester=AWSTester())
+    audit.save_reports()
+    """
+
+    #run_audit(audit, , AWSTester(audit))
 
 if __name__ == "__main__":
     main()
--- examples/github/gh_example_1.py
+++ examples/github/gh_example_1.py
@@ -8,7 +8,6 @@
 import os
 from auditops.core.models import Audit, AuditHelpers
 from auditops.providers.github import GitHubCollector, GitHubTester
-from auditops.core.utils import run_audit
 from dotenv import load_dotenv
 
 def main():
@@ -16,12 +15,14 @@ def main():
 
     helpers = AuditHelpers.create()
 
-    audit = Audit(helpers = helpers, title = "GitHub Audit Report", auditor_name = "AJ Dehn", evidence_folder = "github")
+    # NOTE: Setting summary_mode to 'True' will anonymize the sample ID's in the PDF report (ex. "Sample 1", "Sample 2", "Sample 3").
+    # NOTE: Setting delete_cached_evidence to 'True' will allow you to use previously collected evidence.
+    audit = Audit(helpers = helpers, title = "GitHub Audit Report", auditor_name = "AJ Dehn", evidence_folder = "github",
+    summary_mode = False, delete_cached_evidence= True)
 
-    run_audit(
-        audit,
-        GitHubCollector(os.getenv("github_token"), os.getenv("github_org_name"), audit),
-        GitHubTester(os.getenv("github_org_name"), audit)
+    audit.run(
+        collector=GitHubCollector(os.getenv("github_token"), os.getenv("github_org_name")),
+        tester=GitHubTester()
     )
 
 if __name__ == "__main__":
