3b7fa4daa9a9ab4e34d33084098b6efccbfb8c09
AJ Dehn <ajdehn76@gmail.com>
2026-07-31 11:33:58 -0500

    Moving Publisher and replacing Uploader.

--- src/auditops/core/__init__.py
+++ src/auditops/core/__init__.py
@@ -1,9 +1,6 @@
-__version__ = "0.1.1"
-
 from .evidence.writer import EvidenceWriter
 from .evidence.reader import EvidenceReader
 from .models import Test, Audit, AuditHelpers
-from .uploader import Uploader
 from .reporting.pdf_report_builder import PDFReportBuilder
 from .exclusions import ExclusionManager
 
@@ -11,7 +8,6 @@ from .exclusions import ExclusionManager
 __all__ = [
     "EvidenceWriter",
     "EvidenceReader",
-    "Uploader",
     "PDFReportBuilder",
     "Test",
     "ExclusionManager",
