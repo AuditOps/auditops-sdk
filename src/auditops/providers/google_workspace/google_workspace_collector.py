58ee11b08984b93893afed9f1559642f5f35745b
AJ Dehn <ajdehn76@gmail.com>
2026-08-10 18:02:13 -0500

    Updating GW Constructor

--- src/auditops/providers/google_workspace/google_workspace_collector.py
+++ src/auditops/providers/google_workspace/google_workspace_collector.py
@@ -7,11 +7,23 @@ class GoogleWorkspaceCollector:
         self.credentials_file = credentials_file_path
         self.admin_email = admin_email
 
-    def collect(self, writer):
-        self._collect_org_settings(writer)
+        self.audit_folder = None
+        self.writer = None
+        self.reader = None
 
-    def _collect_org_settings(self, writer):
-        scopes = ["https://www.googleapis.com/auth/admin.directory.user.readonly"]
+    def gather_evidence(self, audit):
+        self.audit_folder = audit.audit_folder
+        self.writer = audit.writer
+        self.reader = audit.reader
+
+        self._collect_org_settings()
+
+    def _collect_org_settings(self):
+        scopes = [
+            "https://www.googleapis.com/auth/admin.directory.user.readonly",
+            "https://www.googleapis.com/auth/admin.directory.group.readonly",
+            "https://www.googleapis.com/auth/admin.directory.group.member.readonly",
+        ]
 
         # Authenticate using the service account credentials
         creds = service_account.Credentials.from_service_account_file(
@@ -22,7 +34,34 @@ class GoogleWorkspaceCollector:
         # Build the Directory API client
         service = build("admin", "directory_v1", credentials=delegated_creds)
 
+        self._save_google_users(service)
+        self._save_google_groups(service)
+
+    def _save_google_users(self, service):
         # Save Users File
         response = service.users().list(customer="my_customer").execute()
         users_list = response.get("users", [])
-        writer.save_json("google_workspace", "admin/users.json", users_list)
+        self.writer.save_json(f"{self.audit_folder}/audit_evidence/admin/users.json", users_list)
+
+    def _save_google_groups(self, service):
+        groups = []
+        page_token = None
+
+        while True:
+            response = (
+                service.groups()
+                .list(
+                    customer="my_customer",
+                    maxResults=200,
+                    pageToken=page_token,
+                )
+                .execute()
+            )
+
+            groups.extend(response.get("groups", []))
+
+            page_token = response.get("nextPageToken")
+            if not page_token:
+                break
+        
+        self.writer.save_json(f"{self.audit_folder}/audit_evidence/admin/groups.json", groups)
